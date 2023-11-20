e8f6e1ab36ac2352d104620ea254e285bbf43ff6
EmanueleGranillo <granilloemanuele@gmail.com>
2023-11-20 19:04:27 +0100

    XML per creazione aste

---
+++ app/src/main/java/com/example/dietideals24/AstaAlRibassoActivity.java
@@ -0,0 +1,14 @@
+package com.example.dietideals24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AstaAlRibassoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asta_al_ribasso);
+    }
+}
