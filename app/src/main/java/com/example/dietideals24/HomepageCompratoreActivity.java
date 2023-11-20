5b5a4588e93aa79ccc1b2adfc9938c15c4ef22fb
EmanueleGranillo <granilloemanuele@gmail.com>
2023-11-20 19:47:04 +0100

    Creazione altri XML

---
+++ app/src/main/java/com/example/dietideals24/HomepageCompratoreActivity.java
@@ -0,0 +1,14 @@
+package com.example.dietideals24;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomepageCompratoreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage_compratore);
+    }
+}
