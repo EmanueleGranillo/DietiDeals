4f910fd693ca41dd5069b855cb4db0b674c51ed9
boscus00 <boscus.now2000@gmail.com>
2023-11-21 10:19:51 +0100

    Added List and single row list personalization

---
+++ app/src/main/java/com/example/dietideals24/CustomBaseAdapter.java
@@ -0,0 +1,51 @@
+package com.example.dietideals24;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomBaseAdapter extends BaseAdapter {
+
+     Context context;
+     String notificationsTitleList[];
+     int checkOrXIcons[];
+     LayoutInflater inflater;
+
+     public CustomBaseAdapter(Context ctx, String[] notificationsTitleList, int [] checkOrXIcons) {
+         this.context = ctx;
+         this.notificationsTitleList = notificationsTitleList;
+         this.checkOrXIcons = checkOrXIcons;
+         inflater = LayoutInflater.from(ctx);
+     }
+
+
+    @Override
+    public int getCount() {
+        return notificationsTitleList.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+         convertView = inflater.inflate(R.layout.activity_single_row_list_notifications, null);
+         TextView textViewTitle = (TextView) convertView.findViewById(R.id.textViewTitle);
+         ImageView checkOrXImages = (ImageView) convertView.findViewById(R.id.imageIconCheck);
+         textViewTitle.setText(notificationsTitleList[position]);
+         checkOrXImages.setImageResource(checkOrXIcons[position]);
+         return convertView;
+    }
+}
--- app/src/main/java/com/example/dietideals24/NotificationsActivity.java
+++ app/src/main/java/com/example/dietideals24/NotificationsActivity.java
@@ -6,13 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class NotificationsActivity extends AppCompatActivity {
+    String notificationsTitleList[] = {"Hai vinto", "Hai perso"};
+    int checkOrXIcons[] = {R.drawable.ic_check_foreground, R.drawable.ic_x_foreground};
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notifications);
+
+        listView = (ListView) findViewById(R.id.customListView);
+        CustomBaseAdapter customBaseAdapter = new CustomBaseAdapter(getApplicationContext(), notificationsTitleList, checkOrXIcons);
+        listView.setAdapter(customBaseAdapter);
 
         Button backBtn = findViewById(R.id.backButtonNotifiche);
         backBtn.setOnClickListener(new View.OnClickListener() {
