33c4b3d88820b4d80d33077a6919a59fd3c2626d
boscus00 <boscus.now2000@gmail.com>
2023-11-22 21:12:09 +0100

    almost end asta's lists and made class asta (da completare)

--- app/src/main/java/com/example/dietideals24/CustomBaseAdapterProducts.java
+++ app/src/main/java/com/example/dietideals24/CustomBaseAdapterProducts.java
@@ -5,21 +5,27 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class CustomBaseAdapterProducts extends BaseAdapter {
     Context context;
-    Asta listaAsta[];
+    ArrayList<Asta> aste;
+    int productsImages[];
     LayoutInflater inflater;
 
-    public CustomBaseAdapterProducts (Context ctx, Asta[] asta){
+    public CustomBaseAdapterProducts (Context ctx, ArrayList<Asta> aste, int [] productsImages){
         this.context = ctx;
-        listaAsta = asta;
+        this.aste = aste;
+        this.productsImages = productsImages;
         inflater = LayoutInflater.from(ctx);
     }
 
     @Override
     public int getCount() {
-        return listaAsta.length;
+        return aste.size();
     }
 
     @Override
@@ -34,6 +40,50 @@ public class CustomBaseAdapterProducts extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if(aste.get(position).getTipologia().equals("Asta a tempo fisso")){
+            convertView = inflater.inflate(R.layout.activity_custom_list_view_product_t_f, null);
+            TextView titolo = (TextView) convertView.findViewById(R.id.titleTF);
+            TextView tipologia = (TextView) convertView.findViewById(R.id.tipologiaAstaValueTF);
+            TextView data = (TextView) convertView.findViewById(R.id.expirationTextViewValueTF);
+            TextView prezzoAttuale = (TextView) convertView.findViewById(R.id.valoreCorrenteTF);
+            ImageView productImage = (ImageView) convertView.findViewById(R.id.imageProduct);
+            titolo.setText(aste.get(position).getTitoloAsta());
+            tipologia.setText(aste.get(position).getTipologia());
+            // data.setText(aste.get(position).getDataFineAstaTempoFisso()); problemi con il parse, poi si vede
+            prezzoAttuale.setText(aste.get(position).getOffertaAttuale().toString() + "$");
+            productImage.setImageResource(productsImages[position]);
+        }
+        if(aste.get(position).getTipologia().equals("Asta all'inglese")){
+            convertView = inflater.inflate(R.layout.activity_custom_list_view_product_english, null);
+            TextView titolo = (TextView) convertView.findViewById(R.id.titleIng);
+            TextView tipologia = (TextView) convertView.findViewById(R.id.tipologiaAstaValueIng);
+            TextView prezzoAttuale = (TextView) convertView.findViewById(R.id.valoreCorrenteIng);
+            TextView tempoRimanente = (TextView) convertView.findViewById(R.id.timeLeftValueIng);
+            TextView sogliaRialzo = (TextView) convertView.findViewById(R.id.sogliaRialzoValueIng);
+            ImageView productImage = (ImageView) convertView.findViewById(R.id.imageProductIng);
+            titolo.setText(aste.get(position).getTitoloAsta());
+            tipologia.setText(aste.get(position).getTipologia());
+            prezzoAttuale.setText(aste.get(position).getOffertaAttuale().toString() + "$");
+            tempoRimanente.setText(aste.get(position).getTimer().toString());
+            sogliaRialzo.setText(aste.get(position).getSogliaRialzoMinima().toString() + "$");
+            productImage.setImageResource(productsImages[position]);
+        }
+        if(aste.get(position).getTipologia().equals("Asta al ribasso")){
+            convertView = inflater.inflate(R.layout.activity_custom_list_view_product_english, null);
+            TextView titolo = (TextView) convertView.findViewById(R.id.titleRibasso);
+            TextView tipologia = (TextView) convertView.findViewById(R.id.tipologiaAstaValueRibasso);
+            TextView prezzoAttuale = (TextView) convertView.findViewById(R.id.valoreCorrenteRibasso);
+            TextView tempoRimanente = (TextView) convertView.findViewById(R.id.decrementoTimerValueRibasso);
+            TextView sogliaDecremento = (TextView) convertView.findViewById(R.id.sogliaDecrementoValueRibasso);
+            ImageView productImage = (ImageView) convertView.findViewById(R.id.imageProductRibasso);
+            titolo.setText(aste.get(position).getTitoloAsta());
+            tipologia.setText(aste.get(position).getTipologia());
+            prezzoAttuale.setText(aste.get(position).getPrezzoVendita().toString() + "$");
+            tempoRimanente.setText(aste.get(position).getTimer().toString());
+            sogliaDecremento.setText(aste.get(position).getImportoDecremento().toString() + "$");
+            productImage.setImageResource(productsImages[position]);
+        }
+        return convertView;
     }
 }
--- app/src/main/java/com/example/dietideals24/HomepageCompratoreActivity.java
+++ app/src/main/java/com/example/dietideals24/HomepageCompratoreActivity.java
@@ -6,11 +6,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.math.BigDecimal;
 
 public class HomepageCompratoreActivity extends AppCompatActivity {
 
+    ArrayList<Asta> aste;
+    Asta astaTempoFisso;
+    Asta astaInglese;
+    Asta astaRibasso;
+    int productsImages [] = {R.drawable.macbookpro, R.drawable.casadiriposo, R.drawable.bottigliaacqua };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //CREAZIONI ASTE PER PROVARE
+        //ASTA A TEMPO FISSO
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Date dataFineAstaTempoFisso;
+        try {
+            dataFineAstaTempoFisso = dateFormat.parse("2024-01-12");
+            astaTempoFisso = new Asta("MACBOOK PRO", "Asta a tempo fisso", dataFineAstaTempoFisso, new BigDecimal(30), new BigDecimal(100));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        //ASTA ALL'INGLESE
+        astaInglese = new Asta("CASA DI RIPOSO", "Asta all'inglese", new BigDecimal(50), new BigDecimal(5), 40000);
+
+        //ASTA AL RIBASSO
+        astaRibasso = new Asta("BOTTIGLIA ACQUA", "Asta al ribasso", new BigDecimal(100), 50000, new BigDecimal(10), new BigDecimal(50));
+
+        aste = new ArrayList<Asta>();
+        aste.add(astaTempoFisso);
+        aste.add(astaInglese);
+        aste.add(astaRibasso);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage_compratore);
 
