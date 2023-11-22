5cc02fce580a2ae28d91139bd8d17b603bf32f79
Pietro <pietro.carnevale.01@gmail.com>
2023-11-22 11:40:32 +0100

    Update MainActivity.java
    compra vendi btn

--- app/src/main/java/com/example/dietideals24/MainActivity.java
+++ app/src/main/java/com/example/dietideals24/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.dietideals24;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button compraBtn;
+    Button vendiBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
         TextView registratiTxt = findViewById(R.id.registratiTextButton);
         Button accediBtn = findViewById(R.id.accediButton);
+        compraBtn = findViewById(R.id.compraButtonLogin);
+        vendiBtn = findViewById(R.id.vendiButtonLogin);
         registratiTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent goToProfilo = new Intent(MainActivity.this, HomepageCompratoreActivity.class);
                 startActivity(goToProfilo);
+            }
+        });
+
+        vendiBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                vendiBtn.setBackgroundColor(Color.parseColor("#00CC66"));
+                compraBtn.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                vendiBtn.setTextColor(Color.parseColor("#FFFFFF"));
+                compraBtn.setTextColor(Color.parseColor("#000000"));
+            }
+        });
+
+        compraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                compraBtn.setBackgroundColor(Color.parseColor("#00CC66"));
+                vendiBtn.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                compraBtn.setTextColor(Color.parseColor("#FFFFFF"));
+                vendiBtn.setTextColor(Color.parseColor("#000000"));
             }
         });
     }
