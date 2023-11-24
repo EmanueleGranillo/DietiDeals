334168d1e3247f1b8e823af2b79ad9a9465270d7
Pietro <pietro.carnevale.01@gmail.com>
2023-11-24 10:10:03 +0100

    small fix
    arrow crea asta fixed

--- app/src/main/java/com/example/dietideals24/CreateAstaPT1.java
+++ app/src/main/java/com/example/dietideals24/CreateAstaPT1.java
@@ -1,17 +1,28 @@
 package com.example.dietideals24;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.github.dhaval2404.imagepicker.ImagePicker;
+
 public class CreateAstaPT1 extends AppCompatActivity {
+
+    ImageView uploadImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_asta_pt1);
+
+        uploadImage = findViewById(R.id.uploadImageIcon);
 
         // Recupera il riferimento allo Spinner dal layout
         Spinner spinnerTipologia = findViewById(R.id.spinnerAuctionType);
@@ -41,5 +52,23 @@ public class CreateAstaPT1 extends AppCompatActivity {
         // Applica l'adapter allo Spinner
         spinnerCategoria.setAdapter(adapterCategorie);
 
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImagePicker.with(CreateAstaPT1.this)
+                        .crop()	    			//Crop image(Optional), Check Customization for more option
+                        .compress(1024)			//Final image size will be less than 1 MB(Optional)
+                        .maxResultSize(1080, 1080)	//Final image resolution will be less than 1080 x 1080(Optional)
+                        .start();
+            }
+        });
+
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Uri uri = data.getData();
+        uploadImage.setImageURI(uri);
+    }
+
 }
