3db8a0b29c0d7d29c7e704b9cb335cb7f49f86f8
boscus00 <boscus.now2000@gmail.com>
2023-11-28 11:43:14 +0100

    Calendar + time picker

--- app/src/main/java/com/example/dietideals24/activities/CreaAstaIngleseActivity.java
+++ app/src/main/java/com/example/dietideals24/activities/CreaAstaIngleseActivity.java
@@ -6,15 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.example.dietideals24.R;
 
 public class CreaAstaIngleseActivity extends AppCompatActivity {
 
+    NumberPicker numberPickerHoursIng;
+    NumberPicker numberPickerMinutesIng;
+    TextView textViewTimerInsertedIng;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_asta_inglese);
+
+
+        numberPickerHoursIng = findViewById(R.id.numberPickerHoursIng);
+        numberPickerMinutesIng = findViewById(R.id.numberPickerMinutesIng);
+        textViewTimerInsertedIng = findViewById(R.id.textViewTimerInsertedIng);
+
+        // Aggiungi un listener ai NumberPicker per rilevare i cambiamenti
+        numberPickerHoursIng.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                updateTimerText();
+            }
+        });
+
+        numberPickerMinutesIng.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                updateTimerText();
+            }
+        });
+
 
         Button backButtonAstaIng = findViewById(R.id.backButtonCreateAstaInglese);
         backButtonAstaIng.setOnClickListener(new View.OnClickListener() {
@@ -26,7 +53,7 @@ public class CreaAstaIngleseActivity extends AppCompatActivity {
         });
 
 
-        Button createButtonAstaInglese = findViewById(R.id.creaButtonAstaInglese);
+        Button createButtonAstaInglese = findViewById(R.id.creaButtonAstaIng);
         createButtonAstaInglese.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,4 +63,19 @@ public class CreaAstaIngleseActivity extends AppCompatActivity {
         });
 
     }
+
+
+
+    // Funzione per aggiornare la TextView con le ore e i minuti selezionati
+    private void updateTimerText() {
+        int hours = numberPickerHoursIng.getValue();
+        int minutes = numberPickerMinutesIng.getValue();
+
+        // Crea la stringa nel formato desiderato (puoi personalizzarla a tuo piacimento)
+        String timerText = "Timer inserito: " + hours + " ore " + minutes + " minuti";
+
+        // Aggiorna il testo della TextView
+        textViewTimerInsertedIng.setText(timerText);
+    }
+
 }
--- app/src/main/java/com/example/dietideals24/activities/CreaAstaRibassoActivity.java
+++ app/src/main/java/com/example/dietideals24/activities/CreaAstaRibassoActivity.java
@@ -6,15 +6,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 import com.example.dietideals24.R;
 
+
+
 public class CreaAstaRibassoActivity extends AppCompatActivity {
+
+    NumberPicker numberPickerHours;
+    NumberPicker numberPickerMinutes;
+    TextView textViewTimerInsertedRibasso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_asta_ribasso);
+
+        numberPickerHours = findViewById(R.id.numberPickerHours);
+        numberPickerMinutes = findViewById(R.id.numberPickerMinutes);
+        textViewTimerInsertedRibasso = findViewById(R.id.textViewTimerInsertedRibasso);
+
+        // Aggiungi un listener ai NumberPicker per rilevare i cambiamenti
+        numberPickerHours.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                updateTimerText();
+            }
+        });
+
+        numberPickerMinutes.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+                updateTimerText();
+            }
+        });
 
 
         Button backButtonAstaRibasso = findViewById(R.id.backButtonCreateAstaRibasso);
@@ -38,4 +65,17 @@ public class CreaAstaRibassoActivity extends AppCompatActivity {
 
 
     }
+
+    // Funzione per aggiornare la TextView con le ore e i minuti selezionati
+    private void updateTimerText() {
+        int hours = numberPickerHours.getValue();
+        int minutes = numberPickerMinutes.getValue();
+
+        // Crea la stringa nel formato desiderato (puoi personalizzarla a tuo piacimento)
+        String timerText = "Timer inserito: " + hours + " ore " + minutes + " minuti";
+
+        // Aggiorna il testo della TextView
+        textViewTimerInsertedRibasso.setText(timerText);
+    }
+
 }
